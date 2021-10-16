abb694c1a1410bd902756ab5284d41501450b813
Rrrruin <wangkairui0108@gmail.com>
2021-10-16 23:56:08 +0200

    SW_to_String V3 # add function about compare

--- main.py
+++ main.py
@@ -5,9 +5,10 @@
 # @software: PyCharm
 
 import numpy as np
+import pandas    as pd
 
 
-def Smith_Waterman(str1, str2, s_score, m_score):
+def Smith_Waterman(str1, str2):  # s_score, m_score
     len1, len2 = len(str1), len(str2)
 
     # initialize the socre matrix
@@ -19,15 +20,16 @@ def Smith_Waterman(str1, str2, s_score, m_score):
 
     Space_punish = 0
 
-    #score the matrix
+    # score the matrix
     for i in range(1, len1 + 1):
         for j in range(1, len2 + 1):
-            Skj = matrix[i-1, j] - Space_punish
-            Sik = matrix[i, j-1] - Space_punish
-            Sij = matrix[i-1, j-1] + 3 if str1[i-1] == str2 [j-1] else matrix[i-1, j-1] - 3
-            matrix[i,j] = max(Skj, Sik, Sij, 0)
+            Skj = matrix[i - 1, j] - Space_punish
+            Sik = matrix[i, j - 1] - Space_punish
+            Sij = matrix[i - 1, j - 1] + 3 if str1[i - 1] == str2[j - 1] else matrix[i - 1, j - 1] - 3
+            matrix[i, j] = max(Skj, Sik, Sij, 0)
     match_str1, match_str2, match_rate = Trace_back(str1, str2, matrix, Space_punish)
     return match_str1, match_str2, match_rate
+
 
 def Trace_back(str1, str2, M, Space_punish):
     x, y = np.where(M == np.max(M))
@@ -37,37 +39,52 @@ def Trace_back(str1, str2, M, Space_punish):
     match_count = 0
     score = 0
     count = 0
-    while M[x,y] !=0 :
+    while M[x, y] != 0:
         count += 1
         if M[x - 1, y] - Space_punish == M[x, y]:
             x = x - 1
             match_str1, match_str2 = str1[x] + match_str1, '_' + match_str2
 
             score += 0.5
-        elif M[x, y-1] - Space_punish == M[x, y]:
+        elif M[x, y - 1] - Space_punish == M[x, y]:
             y = y - 1
 
             match_str1, match_str2 = '_' + match_str1, str2[y] + match_str2
             score += 0.5
         else:
-            x, y = x-1, y-1
+            x, y = x - 1, y - 1
             match_str1, match_str2 = str1[x] + match_str1, str2[y] + match_str2
             match_count += 1
             score += 1
     return match_str1, match_str2, score / count
+
+def paper_compare(paper_list):
+
+    for i in range(len(paper_list)-1):
+        count = 0
+        for j in range(i+1, len(paper_list)):
+
+            Smith_Waterman(paper_list[i], paper_list[j])
+            count += 1
+        print("paper", i+1, "比较了", count, "次")
+
+        #存数据
+
+
 
 if __name__ == '__main__':
     with open("paper1.txt") as f:
         str1 = f.read()
     with open("paper2.txt") as f:
         str2 = f.read()
-    #print(Smith_Waterman(str1, str2, 0.5, 1))
-    repeat = ""
-    for i in str1 :
-        if i == "_":
-            continue
-        else:
-            repeat += i
+    with open("paper3.txt") as f:
+        str3 = f.read()
+    paper_list = [str1, str2, str3]
+    paper_compare(paper_list)
 
-    print(repeat)
+    #data = [['paper1', repeat1, score]]
+    #paper1 = pd.DataFrame(data, columns=['name', 'rep_to_p2', 'score'])
+    #print(paper1)
+
+
     # implement of Smith-Waterman
